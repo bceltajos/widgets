5fae6328ef74fc37a649a4c845d763d85039f319
bceltajos <bceltajos@gmail.com>
2020-07-29 21:11:17 -0500

    search fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -23,10 +23,20 @@ const Search = () => {
             setResults(data.query.search);
         };
 
-        if(term) {
-            search();            
-        }
-        
+        if (term && !results.length) {
+            search();
+        }else {
+            const timeoutId = setTimeout(() => {
+                if(term) {
+                    search();
+                }
+            }, 1000);        
+            
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        }   
+
     }, [term]);
 
     const renderedResults = results.map((result) => {
